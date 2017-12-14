c67190b6333c02caf191f5b90c24c32fbc6b8679
Desiree Tien <desireetien@Aphesis.local>
2017-12-14 12:47:43 -0600

    Confirming connection

--- Bamazon.js
+++ Bamazon.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -6,14 +7,45 @@ var connection = mysql.createConnection({
 
   // Your username
   user: "root",
-
   // Your password
   password: "root",
-  database: "Bamazon_DB"
+  database: "bamazon_DB"
 });
 
+// Setting up connection function
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-  createProduct();
 });
+
+// Setting up display products function
+
+// function readProducts() {
+//   console.log("Selecting all products...\n");
+//   connection.query("SELECT * FROM products", function(err, res) {
+//     if (err) throw err;
+//     // Log all results of the SELECT statement
+//     console.log(res);
+// }
+
+// start();
+
+// // function which prompts the user for what action they should take
+// function start() {
+//   inquirer
+//     .prompt({
+//       name: "idRequested",
+//       type: "rawlist",
+//       message: "What is the [ID] of item you would like to purchase?",
+//       choices: ["POST", "BID"]
+//     })
+//     .then(function(answer) {
+//       // based on their answer, either call the bid or the post functions
+//       if (answer.postOrBid.toUpperCase() === "POST") {
+//         postAuction();
+//       }
+//       else {
+//         bidAuction();
+//       }
+//     });
+// }
