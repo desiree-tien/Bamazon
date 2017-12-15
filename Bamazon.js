82515063255e0149cd838ce6e70029c607020b4f
Desiree Tien <desireetien@Aphesis.local>
2017-12-15 14:07:38 -0600

    Pushing complted code

--- Bamazon.js
+++ Bamazon.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var = inStock;
+// var = inStock;
 var connection = mysql.createConnection({
   host: "localhost",
   port: 8889,
@@ -12,60 +12,107 @@ var connection = mysql.createConnection({
   database: "bamazon_DB"
 });
 
-// Setting up connection function
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-  readProducts();
-});
+// // Setting up connection function
+// connection.connect(function(err) {
+//   if (err) throw err;
+//   console.log("connected as id " + connection.threadId + "\n");
+//   readProducts();
+// });
 
 // Setting up display products function
 
-function readProducts() {
-  console.log("Selecting all products...\n");
-  connection.query("SELECT * FROM products", function(err, res) {
-    if (err) throw err;
-  console.log(res); 
-  start();
-  })
+connection.connect(function(err) {
+	if (err) throw err;
+	home();
+});
+
+function home() {
+	inquirer.prompt([
+		{
+			name: 'order',
+			type: 'list',
+			message: 'Hello! Welcome to our shop, are you interested in browsing items for sale?',
+			choices: ['Yes!', 'No, Thank you!']
+		}
+	]).then(function(answer) {
+		if (answer.order == 'Yes!') {
+			shop();
+		} else {
+			connection.end();
+			console.log('\n Well alright then! Have a great day!\n');
+		}
+	});
+}
 
 
+function shop() {
+	console.log('\nItems for sale\n');
+	connection.query('SELECT * FROM products', function(err, res) {
+		if (err) {
+			console.log(err)
+		} else {
+			for (var i = 0; i < res.length; i++) {
+				console.log('ID: ' + res[i].id);
+				console.log('Item: ' + res[i].product_name);
+				console.log('Price: ' + res[i].price);
+				console.log('Units in stock: ' + res[i].stock_quantity);
+				console.log('Department : ' + res[i].department_name + '\n');
+				console.log('--------------------------------------------\n');
+			}
 
-// function which prompts the user for what action they should take
-function start() {
-  inquirer.prompt([
-    {
-      name: "idRequested",
-      type: "list",
-      message: "What is the [ID] of item you would like to purchase?",
-      choices: ["1","2","3","4","5","6","7","8","9","10"]
-    },
-    {
-    type: "input",
-    name: "number",
-    message: "How many of this item would you like?"
-    },
-  ])
-    .then(function(user) {
-      // based on their answer, either call the bid or the post functions
-      console.log("You are buying " + user.number + "qty of item number #" + user.idRequested), 
-      updateProduct();
-    
+			inquirer.prompt([
+				{
+					name: 'idRequested',
+					type: 'input',
+					message: '\nWhat is the [ID] of the item you would like to purchase?'
+				},
+				{
+	    			name: 'numbers',
+	    			type: 'input',
+	    			message: 'How many of this item would you like to buy?'
+	   		 	}
+			]).then(function(answer) {
+				if (answer.idRequested != '' && answer.numbers != '') {
+					inStock = res[(answer.idRequested - 1)].stock_quantity;
+	    			checkout(answer.idRequested, answer.numbers);
+	    		} else {
+	    			console.log('\nPlease answer all fields\n');
+	    			shopMore();
+	    		}
+			});
+		}
+	});
+};
 
-    function updateProduct() {
-      console.log("Placing Order....\n");
-      var query = connection.query(
-        "UPDATE products SET ? WHERE ?",
-        [
-          {
-            stock_quantity: user.number
-          },
-          {
-            id: (inStock - user.idRequested)
-          }
-        ],
-        function(err, res) {
-          console.log("Order for " + user.idRequested + " confirmed!\n");
-          console.log("Only "+ res.affectedRows + " reamining!")
-        })};
-        })}};
+function checkout(item, quantity) {
+	if (quantity <= inStock) {
+		var query = 'UPDATE products SET stock_quantity = ' + (inStock - quantity) + ' WHERE id = ' + item;
+		connection.query(query, function(err, res) {
+			if (err) throw err;
+			console.log('\n=================================');
+			console.log('\nYou ordered: ' + quantity + ' units of the item with an ID of ' + item + '.\n');
+			shopMore();
+		});
+	} else {
+		console.log("Whoops! We are out of that item, please try another!");
+		shop();
+	}
+};
+
+function shopMore() {
+	inquirer.prompt([
+		{
+			name: 'again',
+			type: 'confirm',
+			message: 'Would you like to continue shopping?'
+		}
+	]).then(function(answer) {
+		if (answer.again === true) {
+			shop();
+		} else {
+			connection.end();
+			console.log('\nThank you for shopping with us!\n');
+		}
+	});
+};
+
